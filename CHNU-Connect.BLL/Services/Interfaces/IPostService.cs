49fcd8c162f5a5b1d278b7079b2bdc50459666ba
Yaroslavcs <sapsai.yaroslav@chnu.edu.ua>
2025-12-11 18:00:54 +0200

    Add files via upload

--- CHNU-Connect.BLL/Services/Interfaces/IPostService.cs
+++ CHNU-Connect.BLL/Services/Interfaces/IPostService.cs
@@ -13,12 +13,13 @@ namespace CHNU_Connect.BLL.Services.Interfaces
         Task<PostDto?> GetByIdAsync(int id);
         Task<IEnumerable<PostDto>> GetAllAsync();
         Task<IEnumerable<PostDto>> GetByUserIdAsync(int userId);
-        Task<PostDto> UpdatePostAsync(int id, CreatePostDto dto);
+        Task<PostDto> UpdatePostAsync(int id, UpdatePostDto dto);
         Task<bool> DeletePostAsync(int id);
         Task<bool> LikePostAsync(int postId, int userId);
         Task<bool> UnlikePostAsync(int postId, int userId);
         Task<int> GetLikeCountAsync(int postId);
 
         Task<IEnumerable<PostDto>> GetFeedAsync(int? page = 1, int pageSize = 10);
+        Task<IEnumerable<PostDto>> SearchPostsAsync(string searchTerm);
     }
 }
